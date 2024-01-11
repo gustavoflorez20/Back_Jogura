246fc70c98d9811d4c192e139e9ad32fd4392865
Gustavo <148284090+gustavoflorez20@users.noreply.github.com>
2024-01-12 00:24:23 +0100

    mongo clave

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
+
 require('dotenv').config();
-const express = require('express')
-const mongoose  = require ('mongoose')
+const express = require('express');
+const mongoose = require('mongoose');
 
+const app = express();
+const port = process.env.PORT || 3001;
+const cors = require('cors');
 
-
-const app = express()
-const port = 3001
-const cors = require("cors");
 
 
 const ProductsRouter = require('./Back_Products/ProductsRouter/ProductsRouter');
@@ -15,15 +15,14 @@ const UserRouter = require('./Back_User/UserRoutes/UserRouter');
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb+srv://gutavo:Clio2018%2A@cluster0.f8klybc.mongodb.net/Tequetapas')
+mongoose.connect(process.env.MONGODB_URL_PROD)
+.then(() => console.log('Conectada BD de los Productos Jogura http://localhost:3001/Products'))
+.catch((error) => console.error('Hay un error:', error));
 
-  .then(() => console.log('Conectada BD de los Productos Jogura http://localhost:3001/Products'))
-  .catch((error) => console.log('Hay un error:', error));
-
-  const userDBConnection = mongoose.createConnection('mongodb+srv://gutavo:Clio2018*@cluster0.f8klybc.mongodb.net/User');
-  userDBConnection.on('open', () => {
-    console.log('Conectada BD de los Usuarios  Jogura http://localhost:3001/User');
-  });
+const userDBConnection = mongoose.createConnection(process.env.MONGODB_URL_USER);
+userDBConnection.on('open', () => {
+console.log('Conectada BD de los Usuarios  Jogura http://localhost:3001/User');
+});
 
 app.use('/Products', ProductsRouter);
 app.use('/User', UserRouter);
